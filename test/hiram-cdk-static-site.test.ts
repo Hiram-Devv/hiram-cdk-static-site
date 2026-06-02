c0639ff24604529411a083559a7602013c02f3e7
HiramDev <hiramdev02@gmail.com>
2026-06-01 21:47:05 -0600

    Initial commit

---
+++ test/hiram-cdk-static-site.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as HiramCdkStaticSite from '../lib/hiram-cdk-static-site-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/hiram-cdk-static-site-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new HiramCdkStaticSite.HiramCdkStaticSiteStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
